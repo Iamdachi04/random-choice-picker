ad54c7c6545a233695ff48fbfff44e6fd2c0c72a
Iamdachi04 <dachi4github@gmail.com>
2025-08-21 21:38:38 +0400

    created function for the creation of the choices , after this we will have to create a way to choose a random one.

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-const tags = document.querySelector("tags");
+const tagsEl = document.getElementById("tags");
 const textarea = document.getElementById("textarea");
-
+tagsEl.innerHTML='';
 
 textarea.focus()
 
 
+textarea.addEventListener('keyup',(e)=>{
+    createTags(e.target.value)
+})
+
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim()!=='').map(tag=>tag.trim())
+    tagsEl.innerHTML='';
+
+
+    tags.forEach(tag => {
+        const span = document.createElement('span');
+        span.classList.add('tag');
+        span.innerText = tag;
+        tagsEl.appendChild(span);
+    });
+}
