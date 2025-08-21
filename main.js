5caa6fcad9b30f9a1fe5b35978891c6a5bdc038b
Iamdachi04 <dachi4github@gmail.com>
2025-08-21 22:14:04 +0400

    created the function that reaches into the tags , and chooses one of it , and then settimeout applies the changecolor class to the tag that got picked , and finally one of them gets picked and colored.

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ textarea.focus()
 
 textarea.addEventListener('keyup',(e)=>{
     createTags(e.target.value)
+
+
+    if(e.key==="Enter"){
+        setTimeout(() => {
+            e.target.value='';
+            choicepicker();
+        }, 10);
+    }
 })
 
 
@@ -21,4 +29,37 @@ function createTags(input) {
         span.innerText = tag;
         tagsEl.appendChild(span);
     });
+}
+
+function choicepicker() {
+const times = 30;
+const interval = setInterval(() => {
+    const randomTag = picking();
+    highlight(randomTag);
+
+    setTimeout(() => {
+        unhighlight(randomTag);
+    }, 100);
+}, 100);
+
+setTimeout(() => {
+    clearInterval(interval);
+    setTimeout(() => {
+        const randomTag = picking();
+        highlight(randomTag);
+    }, 100);
+}, times * 100);
+}
+
+function picking() {
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlight(tag) {
+    tag.classList.add('changecolor');
+}
+
+function unhighlight(tag) {
+    tag.classList.remove('changecolor');
 }
